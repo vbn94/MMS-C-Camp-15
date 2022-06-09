8279e9cf95371725da67a740fcf104e1bb0f7faa
vbn94 <v.nachev@uktc-bg.com>
2022-06-09 18:04:05 +0300

    Day09

--- Day09/structs-Func.c
+++ Day09/structs-Func.c
@@ -7,22 +7,23 @@
 #define COUNT 10
 
 // #pragma pack(1)
-
-int randint(int,int);
-double randReal(double,double);
-char* randomName(char*);
-
 typedef struct student {
     char name[50];
     uint8_t age;
     double grade;
 } student_t;
 
-void studentInfo(student_t st){
-    printf("Name: %24s, Age: %d, Grade: %.2lf\n", st.name, st.age, st.grade);
-}
+
+int randint(int,int);
+double randReal(double,double);
+char* randomName(char*);
+void studentInfo(student_t);
+int compGrades(const void*, const void*);
+int compAge(const void*, const void*);
+int compName(const void*, const void*);
 
 int main(){
+    srand(time(NULL));
     student_t st1 = { 
         .name="Ivan",
         .age=34,
@@ -42,6 +43,11 @@ int main(){
         mmsStudents[i].age = randint(10, 60);
         mmsStudents[i].grade = randReal(2.0, 6.0);
     }
+
+    // qsort(mmsStudents, COUNT, sizeof(*mmsStudents), compGrades);
+    //qsort(mmsStudents, COUNT, sizeof(*mmsStudents), compAge);
+    qsort(mmsStudents, COUNT, sizeof(*mmsStudents), compName);
+
     for (int i = 0; i < COUNT; i++){
         studentInfo(mmsStudents[i]);
     }
@@ -74,3 +80,31 @@ char* randomName(char* name){
     name[index] = '\0';
     return name;
 }
+
+void studentInfo(student_t st){
+    printf("Name: %24s, Age: %d, Grade: %.2lf\n", st.name, st.age, st.grade);
+}
+
+int compGrades(const void* st1p, const void* st2p){
+    student_t st1 = *(student_t*)st1p;
+    student_t st2 = *(student_t*)st2p;
+    if (fabs(st1.grade - st2.grade) < 0.01){
+        return 0;
+    } else if (st1.grade > st2.grade){
+        return -1;
+    }
+    return 1;
+}
+
+int compAge(const void* st1p, const void* st2p){
+    student_t st1 = *(student_t*)st1p;
+    student_t st2 = *(student_t*)st2p;
+    return st1.age - st2.age;
+}
+
+int compName(const void* st1p, const void* st2p){
+    student_t st1 = *(student_t*)st1p;
+    student_t st2 = *(student_t*)st2p;
+    return strcmp(st1.name, st2.name);
+}
+
