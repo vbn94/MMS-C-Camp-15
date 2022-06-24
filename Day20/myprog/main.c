401a178933d193ac3eae070bcf97757103ac5ff0
vbn94 <v.nachev@uktc-bg.com>
2022-06-24 23:25:46 +0300

    Day20

---
+++ Day20/myprog/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mymath.h"
+
+int main(){
+    int result = sum(10, 20);
+    printf("Result = %d\n", result);
+    return 0;
+}
---
+++ Day20/myprog/mymath.h
@@ -0,0 +1,4 @@
+#ifndef _MYMATH_H
+#define _MYMATH_H
+int sum(int, int);
+#endif
